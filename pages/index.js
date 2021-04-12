bf456863c77fb8307266c0f76d341e3ff50071b5
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-12 22:16:34 +0530

    home-page pieces

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,30 @@ export default function Home() {
 				<title>Create Next App</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-      <h1>Hello Next.js</h1>
+			<h1>Hello Next.js</h1>
+			{/* Header */}
+			<header>
+				{/* Left-side */}
+				<div className=''>
+        <p>About</p>
+        <p>Store</p>
+
+        </div>
+
+				{/* Right-side */}
+				<div className=''>
+					<p>Gmail</p>
+					<p>Images</p>
+
+					{/* Icon */}
+
+					{/* Avatar */}
+				</div>
+			</header>
+
+			{/* Body */}
+
+			{/* Footer */}
 		</div>
-    
 	);
 }
