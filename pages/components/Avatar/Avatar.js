17edf6a857d84d31aa486f1bd5fc434f126b90e1
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-13 07:28:38 +0530

    heroicons-tailwindcss

--- pages/components/Avatar/Avatar.js
+++ pages/components/Avatar/Avatar.js
@@ -3,7 +3,12 @@ import React from 'react';
 function Avatar({ url }) {
 	return (
 		<div>
-			<img loading='lazy' className='h-10' src={url} alt='profile' />
+			<img
+				loading='lazy'
+				className='h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110'
+				src={url}
+				alt='profile'
+			/>
 		</div>
 	);
 }
